8b557a7aebd8c9a666eeffecd1c44400cc0e1368
Naveed Ahmed <42344659+Naveed17@users.noreply.github.com>
2020-06-25 15:33:12 +0500

    push

---
+++ header.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+	
+<!-- Mirrored from www.rajeshdoot.com/demo/niwax/ by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 23 Jun 2020 08:13:09 GMT -->
+<head>
+		<meta charset="utf-8" />
+		<title>Techgater - Web Design &amp; Development Agency</title>
+		<meta name="description" content="Creative Agency, Marketing Agency Template">
+		<meta name="keywords" content="Creative Agency, Marketing Agency, techgater">
+		<meta name="author" content="Kamran Haider">
+		<meta name="viewport" content="width=device-width,initial-scale=1">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="theme-color" content="#c7ecff">
+		<!--website-favicon-->
+		<link rel="icon" type="image/png" href="images/favicon.png">
+		<!--plugin-css-->
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<link href="css/owl.carousel.min.css" rel="stylesheet">
+		<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css" rel="stylesheet">
+		<!--google-fonts-->
+		<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&amp;family=Poppins:wght@400;500;600;700&amp;display=swap" rel="stylesheet">
+		<!-- template-style-->
+		<link type="text/css" href="css/style.css" rel="stylesheet">
+		<link type="text/css" href="css/responsive.css" rel="stylesheet">
+	</head>
+	<body>
+		<!--Start Header -->
+		<header class="top-header">
+			<nav class="navbar navbar-expand-lg navbar-light justify-content-right navbar-mobile fixed-top">
+				<div class="container">
+					<a class="navbar-brand font-weight-bold" href="/"> <img src="images/logo.png" width="200px" class="img-fluid"/></a>
+					<button class="navbar-toggler mobile-none" type="button" data-toggle="collapse" data-target="#navbar4" aria-controls="navbar4" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+					<div class="collapse navbar-collapse animate slideIn mobile-none" id="navbar4">
+					<ul class="mr-auto"></ul>
+					<ul class="navbar-nav d-menu">
+						<li class="nav-item dropdown">
+							<a class="nav-link " href="index.php" id="homepage1" >Home</a>
+						
+						</li>
+						<li class="nav-item"> <a class="nav-link" href="about.php">About </a> </li>
+						<li class="nav-item dropdown">
+							<a class="nav-link" href="service.php" id="Services" >Services</a>
+						</li>
+						<li class="nav-item dropdown">
+							<a class="nav-link" href="portfolio.php" id="Services" >Portfolio</a>
+						</li>
+						<li class="nav-item dropdown">
+							<a class="nav-link" href="pricing.php" id="Pages">Pricing</a>
+						</li>
+						<li class="nav-item dropdown">
+							<a class="nav-link" href="team.php" id="Pages">Our Team</a>
+						</li>
+						<li class="nav-item"> <a class="nav-link custom-btn lnk btn-main bg-btn" href="get-quote.php">Get A Quote <span class="circle"></span></a> </li>
+					</ul>
+				</div>
+				<div class="mobile-menu">
+					<ul class="mob-nav">
+						<li> <a class="nav-link mobilemenu" href="#" data-toggle="modal" data-target="#menu"><i class="fas fa-bars"></i></a> </li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	</header>
+    <!-- hedader End -->
