c72a834b7ed654ae7889e0761b9474b3fa65a73a
Kamran Haider <kamran@phptravels.org>
2020-08-20 22:16:22 +0500

    sadasd

--- airgater/_next/static/chunks/webpack.js
+++ airgater/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "2760836c72f57fba4705";
+/******/ 	var hotCurrentHash = "9828f4de8fe794a00d09";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
